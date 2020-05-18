f7766b15e20f6e915ca5ba3846422838126df3c8
Emmanuel <emmanuelvictor.12@gmail.com>
2020-05-18 03:28:57 -0300

    Week 2 of AceleraDev

--- python-6/main.py
+++ python-6/main.py
@@ -1,38 +1,42 @@
-
-
 class Department:
     def __init__(self, name, code):
         self.name = name
         self.code = code
 
-
 class Employee:
-    def __init__(self, code, name, salary):
+    def employ(self, code, name, salary):
         self.code = code
         self.name = name
         self.salary = salary
-
+        self.carga_horaria = 8
     def calc_bonus(self):
-        pass
-
+        return 0
     def get_hours(self):
-        pass
-
+        return self.carga_horaria
 
 class Manager(Employee):
     def __init__(self, code, name, salary):
-        super().__init__(code, name, salary)
-        self.departament = Department('managers', 1)
-
+        self.employ(code, name, salary)
+        self.__department = Department('managers', 1)
     def calc_bonus(self):
         return self.salary * 0.15
+    def get_departament(self):
+        return self.__department.name
+    def set_department(self,department,code):
+        self.__department = Department(department,code)
 
-
-class Seller(Manager):
+class Seller(Employee):
     def __init__(self, code, name, salary):
-        super().__init__(code, name, salary)
-        self.departament = Department('sellers', 2)
-        self.sales = 0
-
-    def get_hours(self):
-        return 6
+        self.employ(code, name, salary)
+        self.__department=Department('sellers', 2)
+        self.__sales=0
+    def put_sales(self,new_sales):
+        self.__sales = new_sales+self.__sales
+    def get_sales(self):
+        return self.__sales
+    def calc_bonus(self):
+        return self.get_sales()*0.15
+    def get_departament(self):
+        return self.__department.name
+    def set_department(self,department,code):
+        self.__departament = Department(department,code)
